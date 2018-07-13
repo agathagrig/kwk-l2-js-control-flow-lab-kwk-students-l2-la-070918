5cf709979611e46c3c2d26aabcc5970ad967c7df
Agatha Grig <agathagrigorian@gmail.com>
2018-07-13 21:54:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet) {
+ if (feet<= 400) {
+   result = ("This one is on me!")
+   return result
+ }
+  else if (feet>= 2500) {
+     result = ("No can do.")
+     return result
+   }
+   else if (feet>2000) {
+     result = ("I will gladly take your thirty bucks.")
+     return result
+   }
+ }
+ 
+ function ternaryCheckCity(){
+ 
